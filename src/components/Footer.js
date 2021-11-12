4a8e96e3dc36eb5b6fad046b85f0c0b825da3fdc
Jesús Gutierrez <jgutierrezyancan@outlook.com>
2021-11-12 00:30:45 -0500

    2.0.1

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,17 +5,21 @@ class Footer extends Component {
         const style = {
             position: {
                 display: "inline-flex",
-                margin: "1em 0"
+                margin: "1em 0",
+                alignItems: "center"
             },
             image: {
                 margin: "0 1em",
                 width: "2em",
+            },
+            link: {
+                textDecoration: "none"
             }
         }
         return (
             <div style={style.position}>
                 <img style={style.image} src={process.env.PUBLIC_URL + '/img/github.svg'} alt="#"/>
-                <p>https://github.com/JesusGutierez</p>
+                <a style={style.link} href="https://github.com/JesusGutierez">JesusGutierez</a>
             </div>
         );
     }
